239d3cd020ffe50d733bf377b2257e58976c90f2
Govindsing Chavan <123160733+govind2611@users.noreply.github.com>
2023-10-21 05:57:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function debounce(callback, delay, immediate = false) {
-  
-  }
-  
-  module.exports = debounce;
+  let timeoutId;
+
+  return function (...args) {
+    const executeCallback = () => {
+      callback.apply(this, args);
+    };
+
+    clearTimeout(timeoutId);
+
+    if (immediate && !timeoutId) {
+      executeCallback();
+    }
+
+    timeoutId = setTimeout(executeCallback, delay);
+  };
+}
+
+module.exports = debounce;
